ac3b6da62e66770fab6924bf5a54832ae958f045
kosaka_shota <shiyota_kosaka@mail.toyota.co.jp>
2025-03-31 16:25:26 +0900

    add setup

--- BE-app/src/test/java/com/example/roulette/integration/ChoiceIntegrationTest.java
+++ BE-app/src/test/java/com/example/roulette/integration/ChoiceIntegrationTest.java
@@ -1,5 +1,34 @@
 package com.example.roulette.integration;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.example.roulette.repository.choice.ChoiceRepository;
+import com.example.roulette.repository.theme.ThemeRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class ChoiceIntegrationTest {
     
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ThemeRepository themeRepo;
+
+    @Autowired
+    private ChoiceRepository choiceRepo;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @BeforeEach
+    void setUp(){
+        themeRepo.deleteAllTheme();
+        choiceRepo.deleteAllChoice();
+    }
 }
--- BE-app/src/test/java/com/example/roulette/integration/ThemeIntegrationTest.java
+++ BE-app/src/test/java/com/example/roulette/integration/ThemeIntegrationTest.java
@@ -1,10 +1,13 @@
 package com.example.roulette.integration;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.example.roulette.repository.choice.ChoiceRepository;
+import com.example.roulette.repository.theme.ThemeRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
@@ -15,5 +18,17 @@ public class ThemeIntegrationTest {
     private MockMvc mockMvc;
 
     @Autowired
+    private ThemeRepository themeRepo;
+    
+    @Autowired
+    private ChoiceRepository choiceRepo;
+
+    @Autowired
     private ObjectMapper mapper;
+
+    @BeforeEach
+    void setUp(){
+        themeRepo.deleteAllTheme();
+        choiceRepo.deleteAllChoice();
+    }
 }
